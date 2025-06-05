0c269e90d406aeb2029ee9a8041536c6ba139843
Hendry <hduarteyt@gmail.com>
2025-06-05 13:47:09 -0300

    config for celery in windows, creating the task module to send a succesful message when the order is created

--- core/core/celery.py
+++ core/core/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.signals import worker_process_init
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
@@ -11,8 +12,23 @@ app = Celery('core')
 # the configuration object to child processes.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+# Windows specific configuration
+app.conf.update(
+    broker_connection_retry_on_startup=True,
+    worker_pool_restarts=True,
+    task_track_started=True,
+    worker_max_tasks_per_child=1,  # Restart worker processes after each task
+)
+
 # Load task modules from all registered Django apps
 app.autodiscover_tasks()
+
+@worker_process_init.connect
+def worker_process_init_handler(**_):
+    # Reset connection on worker startup
+    from django.db import connections
+    for conn in connections.all():
+        conn.close_if_unusable_or_obsolete()
 
 @app.task(bind=True)
 def debug_task(self):
