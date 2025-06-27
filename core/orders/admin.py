10406dc432a92c7c0961bb1c626a7ec243a58c2a
Hendry <hduarteyt@gmail.com>
2025-06-27 17:59:45 -0300

    creating custon view to see the orders detail, adding a link to the admin site

--- core/orders/admin.py
+++ core/orders/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Order,OrderItem
 from django.utils.safestring import mark_safe
 from django.http import HttpResponse
+from django.urls import reverse
 import csv
 import datetime
 
@@ -38,9 +39,14 @@ order_payment.short_description = 'Stripe payment'
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+
+def order_detail(obj):
+    url = reverse('admin:orders_order_change', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid',order_payment, 'created', 'updated']
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid',order_payment, 'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
